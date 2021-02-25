6b4339b7a396c5f8ea8b9498c5e780ae43ac67e9
elijahawork <64611104+elijahawork@users.noreply.github.com>
2021-02-24 22:54:46 -0600

    Fixed styling

--- src/schema/MusicNoteSchema.ts
+++ src/schema/MusicNoteSchema.ts
@@ -1,9 +1,9 @@
-import NoteName from "../types/NoteName";
+import NoteName from '../types/NoteName';
 
 type MusicNoteSchema = {
   id: number;
   noteName: NoteName;
   noteDuration: number;
   phraseEnd?: number; // the id of the ending note
-}
+};
 export default MusicNoteSchema;
